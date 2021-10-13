7604965af117493a82de3cc950519ed9332ff31b
Anjali Gopinathan <asgopina@usc.edu>
2021-10-13 03:03:33 -0700

    update twilio util file

--- testTwilio.py
+++ testTwilio.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
 print(load_dotenv())
-# from utils import fetch_sms
-# from utils import send_sms
-import utils
-sms = utils.fetch_sms()
+# from twilio import fetch_sms
+# from twilio import send_sms
+import twilio1
+sms = twilio1.fetch_sms()
 print(sms)
 x = next(sms)
 baran = '+13853369470'
@@ -13,6 +13,6 @@ for x in sms:
     print(x.body)
 
 
-sms_send_result = utils.send_sms("Testing", anjali)
-call_result = utils.make_call("Hello! Your loved one's service dog needs your attention! Please check in on them! Also, milky toes has very milky toes.", anjali)
+sms_send_result = twilio1.send_sms("Testing", anjali)
+call_result = twilio1.make_call("Hello! Your loved one's service dog needs your attention! Please check in on them! Also, milky toes has very milky toes.", anjali)
 
