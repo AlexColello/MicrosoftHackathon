10e506c39caa003d06bf13c9dd290d19bb9441f2
Alex Colello <alexlcolello@gmail.com>
2021-10-13 02:47:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    addition = "" if 'msg' not in g else g.msg
-    return 'Last post message: "{}"'.format(addition)
+    return 'Hi hi'
 
 @app.route("/test-post/", methods = ['POST'])
 def returnMessage():
