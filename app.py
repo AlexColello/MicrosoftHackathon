d358a6d9e635adb2b53fe1fa47d68d1069e82a42
Anjali Gopinathan <asgopina@usc.edu>
2021-10-13 03:10:40 -0700

    merge merge app minor changes

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-# this file is local on Anj's computer
-with open("../barkAlert_twilio_secret.txt", "r") as f:
-    TWILIO_SECRET = f.readlines()
-
-TWILIO_ACCOUNT_SID = os.environ["SK387d5a95afb47ad5c1543859bc648216"]
-TWILIO_AUTH_TOKEN = os.environ[TWILIO_SECRET]
-twilio_api = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
-
 @app.route('/',methods = ['POST', 'GET'])
 def hello():
     return 'Hi hi'
