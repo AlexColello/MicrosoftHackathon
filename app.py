c16c18c369cc60e1965c4aeb305e5315fba124b3
Anjali Gopinathan <asgopina@usc.edu>
2021-10-12 23:41:44 -0700

    minor update

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route("/")
+# this file is local on Anj's computer
+with open("../barkAlert_twilio_secret.txt", "r") as f:
+    TWILIO_SECRET = f.readlines()
+
+TWILIO_ACCOUNT_SID = os.environ["SK387d5a95afb47ad5c1543859bc648216"]
+TWILIO_AUTH_TOKEN = os.environ[TWILIO_SECRET]
+twilio_api = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+@app.route('/',methods = ['POST', 'GET'])
 def hello():
     return "Hello, World!!"
+
+
+@app.route("/sms")
+def sendTextToOwner():
+
+    return "possibly just sent a text"
+
+@app.route('/fetch')
+def fetch_sms():
+    return twilio_api.messages.stream()
