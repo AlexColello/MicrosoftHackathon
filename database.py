f5d1fa78a62b30d751968a1261ef5c8ea8879a9f
Alex Colello <alexlcolello@gmail.com>
2021-10-13 05:50:07 -0700

    Add database secrets

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import pyodbc
-
+import os
 from flask import g
 
 
 def get_db():
     if 'db' not in g:
-        g.db = pyodbc.connect(''
+        g.db = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};\
+            Server=' + os.environ['DATABASE_SERVER'] + ';\
+            Database=' + os.environ['DATABASE_ID'] + ';\
+            Uid=' + os.environ['DATABASE_USER_ID'] + ';\
+            Pwd=' + os.environ['DATABASE_PASSWORD'] + ';\
+            Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
         )
 
     return g.db
